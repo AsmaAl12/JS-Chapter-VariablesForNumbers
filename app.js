37f502b8fd9c3dd6d80ec46f8d85f6ec0b76c058
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-04-29 20:41:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// ======= chapter VARIABLES FOR NUMBERS//
+// var myWeight = 44;
+// myNewWeight = myWeight + 3;
+// alert ("my weight is " + myNewWeight);
+
+// var originalNum = 23;
+// var numToBeAdded = 7;
+// var newNum = originalNum + numToBeAdded;
+
+// var myName = "Asma ";
+// var myLastName = 44;
+// var myFullName = myName + myLastName;
+
+// var originalNum = "55";
+// var newNum = originalNum + 66;
+// var totalNumber = newNum + 44;
+
+// var myAgeBeforeTwoYears = 15;
+// var myAgenow = 2;
+// var myAge = myAgeBeforeTwoYears + myAgenow;
+
+// var my1stFoodInADay = 2;
+// var myBreakfast = my1stFoodInADay + " eggs";
+
+
+// var number = 2;
+// var myBreakfast = " eggs";
+// var myDietRoutine = number + myBreakfast;
+
+
+// var myBirthdayGift = "Cake";
+// var myAnotherGift = "Chocolates";
+// var myAllBirthdayGift = myBirthdayGift + " and " + myAnotherGift;
+// alert ( myAllBirthdayGift );
+
+
+// var caseQty = 144;
+// alert ( caseQty );
